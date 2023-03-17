554ddcd2621399d79d41c67e7d30ce04d2d55a4b
Shady Salah <shadysalah297@gmail.com>
2023-03-17 11:15:27 +0100

    Creating 'Round' and 'AllRoundsPerSingleSession' classes

---
+++ lottoland-game-client/src/app/round.ts
@@ -0,0 +1,9 @@
+import { AllRoundsPerSingleSession } from "./rounds-per-single-session";
+
+export class Round {
+
+    roundNumbersPerSingleSession!: number;
+ 
+    allRoundsPerSingleSession!: AllRoundsPerSingleSession;
+
+}
---
+++ lottoland-game-client/src/app/rounds-per-single-session.ts
@@ -0,0 +1,8 @@
+export class AllRoundsPerSingleSession {
+
+    firstPlayerMove!: string;;
+    secondPlayerMove!: string;;
+    roundResult!: string;;
+    restarted!: string;;
+    
+}
