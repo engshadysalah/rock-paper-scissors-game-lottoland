99656867129105a5d7ca8476551e856e3f886d3d
Shady Salah <shadysalah297@gmail.com>
2023-03-17 11:43:55 +0100

    create the round-view component

---
+++ lottoland-game-client/src/app/round-view/round-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { Round } from '../round';
+import { RoundsService } from '../rounds.service';
+
+@Component({
+  selector: 'app-round-view',
+  templateUrl: './round-view.component.html',
+  styleUrls: ['./round-view.component.css']
+})
+export class RoundViewComponent {
+ 
+}
+
