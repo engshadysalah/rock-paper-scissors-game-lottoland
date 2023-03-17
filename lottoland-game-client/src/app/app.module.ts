7b0718a2a36982eab7e0e7b1573a3452bfd079c0
Shady Salah <shadysalah297@gmail.com>
2023-03-17 11:13:09 +0100

    Initializing angular application
    creating the front end with angular

---
+++ lottoland-game-client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RoundViewComponent } from './round-view/round-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoundViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
