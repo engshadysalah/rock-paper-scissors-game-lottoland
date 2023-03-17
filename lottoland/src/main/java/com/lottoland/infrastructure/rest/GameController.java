9a2664f6de94b0db79447b9ed9ce6dba199c358a
Shady Salah <shadysalah297@gmail.com>
2023-03-17 17:28:58 +0100

    create the game controller

---
+++ lottoland/src/main/java/com/lottoland/infrastructure/rest/GameController.java
@@ -0,0 +1,46 @@
+package com.lottoland.infrastructure.rest;
+
+import com.lottoland.application.service.GameService;
+import com.lottoland.domain.api.RoundsPerSingleSessionDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+
+
+
+@RestController
+@RequestMapping(path = "/api/v1/game")
+@CrossOrigin(origins = "http://localhost:4200")
+public class GameController {
+
+	private final GameService gameService;
+
+	public GameController(GameService gameService) {
+		this.gameService = gameService;
+	}
+
+
+	@GetMapping (path = "/play")
+	public RoundsPerSingleSessionDTO playAndGetAllRoundsDetailsPerSingleSession(HttpServletRequest request) {
+
+		String sessionId = request.getSession().getId();
+
+		return gameService.playAndGetAllRoundsDetailsPerSingleSession(sessionId);
+	}
+
+
+	@PostMapping(path = "/restart")
+	public void restartGame(HttpServletRequest request) {
+
+		String sessionId = request.getSession().getId();
+
+		gameService.restartGame(sessionId);
+	}
+
+
+	@GetMapping (path = "/get/allRoundsPerSingleSession")
+	public HashMap<String, RoundsPerSingleSessionDTO> getAllRoundsForAllSessions() {
+
+		return gameService.getAllRoundsForAllSessions();
+	}
+}
